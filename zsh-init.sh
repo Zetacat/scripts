f1aa40739ad57b02e548abb6a4d9e08c8c2e8be6
Po-Hao <k94155@gmail.com>
2024-04-26 17:40:10 +0800

    add zsh-init

---
+++ zsh-init.sh
@@ -0,0 +1,20 @@
+#! /bin/zsh
+
+# run with source
+
+# install oh-my-zsh
+CHSH=no RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# change theme to "minimal"
+sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="minimal"/' ~/.zshrc
+
+# install zsh-autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
+# TODO: change the condition check to a more robust approach
+if ! grep -q "zsh-autosuggestions" ~/.zshrc; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    sed -i '/plugins=(/ { :a; /)/! { N; ba; }; s/)/ zsh-autosuggestions&/ }' ~/.zshrc
+fi
+
+chsh -s $(which zsh)
+source ~/.zshrc
